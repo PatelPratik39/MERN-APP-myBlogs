693de5a421a635c45d05c9c217a9da13e807362d
PatelPratik9 <pratspatel17791@gmail.com>
2024-02-26 13:10:45 -0600

    express file

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
+
 const RegisterPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState("");
+
+    async function register(e) {
+        e.preventDefault();
+        await fetch('http://localhost:4000/register',{
+            method:'POST',
+            body:JSON.stringify({username, password}),
+            headers:{'Content-Type' : 'application/json'},
+        })
+    }
   return (
     <>
-      <form className="register">
+      <form className="register" onSubmit={register}>
         <h1>Register</h1>
-        <input type="text" placeholder="Enter your Username" />
-        <input type="password" placeholder="Enter your password" />
+        <input
+          type="text"
+          placeholder="Enter your Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button>Register</button>
       </form>
     </>
