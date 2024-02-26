851faa48d24e8a4bab9e6701c126edb0e195ab46
PatelPratik9 <pratspatel17791@gmail.com>
2024-02-26 12:42:07 -0600

    shadow effect in form

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,6 +2,7 @@ const LoginPage = () => {
     return (
       <>
         <form className="login">
+          <h1>Login</h1>
           <input type="text" placeholder="Enter your Username" />
           <input type="password" placeholder="Enter your password" />
           <button>Login</button>
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,13 +1,14 @@
 const RegisterPage = () => {
-    return (
-      <>
-        <form className="register">
-          <input type="text" placeholder="Enter your Username" />
-          <input type="password" placeholder="Enter your password" />
-          <button>Register</button>
-        </form>
-      </>
-    );
-}
- 
+  return (
+    <>
+      <form className="register">
+        <h1>Register</h1>
+        <input type="text" placeholder="Enter your Username" />
+        <input type="password" placeholder="Enter your password" />
+        <button>Register</button>
+      </form>
+    </>
+  );
+};
+
 export default RegisterPage;
